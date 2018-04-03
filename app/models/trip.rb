7b07b78ad9d3742825859a0a3f4cea1a5df61d74
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-03 12:30:05 -0700

    Add active record validations for trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,12 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
+  validates :date, presence: true
+  validates_inclusion_of :rating, in: [1..5], allow_nil: true
+  validates :cost, presence: true, numericality: { only_integer: true }
+  validates :driver_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :passenger_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
   def cost_usd
     return (cost/100.00).round(2)
   end

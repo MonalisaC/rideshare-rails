d58a26c80b377c26b65c3d2cce482f0a3775e6c5
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-02 21:33:10 -0700

    update new and edit html pages for passengers, created _form html page with content for new and edit html pages. updated passengers controller with create, edit, update, and destroy methods

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -9,17 +9,49 @@ class PassengersController < ApplicationController
   end
 
   def new
+    @passenger = Passenger.new
   end
 
   def create
+    passenger = Passenger.new(passenger_params)
+    if passenger.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @passenger = Passenger.find_by(id: params[:id])
   end
 
   def update
+    @passenger = Passenger.find_by(id: params[:id])
+    if !@passenger.nil?
+      if @passenger.update(passenger_params)
+        redirect_to passenger_path
+      else
+        render :edit
+      end
+    else
+      redirect_to passengers_path
+    end
   end
 
   def destroy
+    id = params[:id]
+    @passenger = Passenger.find(id)
+    if @passenger
+      @passenger.destroy
+    end
+    redirect_to passengers_path
   end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
+
+
 end

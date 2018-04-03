a2024b849b85dec62bbdf81dfe4e9c78e317adab
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-02 21:57:13 -0700

    added driver's trip details

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,17 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  # TODO:
+
+  def get_completed_trips
+   return self.trips.where.not(rating: nil)
+   # first_book = books_with_year.order(publication_date: :asc).first
+   # return first_book.publication_date
+  end
+
+  # def get_average_rating
+  #   trips = self.get_completed_trips
+  #   return trips.inject(0.0) { |sum, trip| sum + trip.rating } / trips.size
+  # end
+
 end

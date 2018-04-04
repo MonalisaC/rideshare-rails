4f73a9ef4f4996517efe430e24d4755d9c91b1b2
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-04 14:42:12 -0700

    fix indents

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   resources :trips, :drivers, :passengers
 
   patch '/drivers/:id/is_deactivated', to: 'drivers#is_deactivated', as:
-    'mark_is_deactivated'
+  'mark_is_deactivated'
 
-    patch '/passengers/:id/mark_deactivate', to: 'passengers#mark_deactivate', as:
-      'mark_passenger_deactivated'
-
+  patch '/passengers/:id/mark_deactivate', to: 'passengers#mark_deactivate', as:
+  'mark_passenger_deactivated'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

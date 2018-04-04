336c49fbb9b91a731d0b2476c3ae371532bb4b40
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-04 15:16:36 -0700

    update schema to set passenger is available to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404220209) do
+ActiveRecord::Schema.define(version: 20180404221055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20180404220209) do
     t.string "phone_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "is_deactivated"
+    t.boolean "is_deactivated", default: false
   end
 
   create_table "trips", force: :cascade do |t|

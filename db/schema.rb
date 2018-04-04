3d8becbf7594320f1bb8174bacf5e3f8b9e488f5
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-04 15:08:54 -0700

    added default is_deactivated as false for new driver

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404054519) do
+ActiveRecord::Schema.define(version: 20180404220209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20180404054519) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_available"
-    t.boolean "is_deactivated"
+    t.boolean "is_deactivated", default: false
   end
 
   create_table "passengers", force: :cascade do |t|

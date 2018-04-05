e6258bcd4c2fad6ada6d8fea5a7cc3493bbade0c
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-04 21:14:45 -0700

    Add nested resource for passenger new trip method.

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,16 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   resources :trips, :drivers, :passengers
 
+  resources :passengers do
+    resources :trips, only: [:create]
+  end
+
   patch '/drivers/:id/is_deactivated', to: 'drivers#is_deactivated', as:
-    'mark_driver_deactivated'
+  'mark_driver_deactivated'
 
   patch '/passengers/:id/mark_deactivate', to: 'passengers#mark_deactivate', as:
   'mark_passenger_deactivated'
-  
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
